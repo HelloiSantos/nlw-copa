ff5c5adcbdc8d6f52c63398daf576d80595eae28
Helloi Santos <helloi_helloi@hotmail.com>
2022-11-04 14:11:24 -0300

    add newgames

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ function createGame(player1, hour, player2) {
   `
 }
 
-let delay = -0.4;
+let delay = -0.4
 function createCard(date, day, games) {
-  delay = delay + 0.3;
+  delay = delay + 0.3
   return `  
     <div class="card" style="animation-delay: ${delay}s">
       <h2>${date} <span>${day}</span></h2>
@@ -21,17 +21,28 @@ function createCard(date, day, games) {
   `
 }
 
-document.querySelector("#app").innerHTML = `
-<header>
-  <img src="./assets/logo.svg" alt="Logo da NLW" />
-      </header>
-      <main id="cards">
-      ${createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia"))}
-      ${createCard(
-        "28/11",
-        "segunda",
-        createGame("brazil", "13:00", "switzerland")
-      )}
-      ${createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))}
-    </main>
-`
+document.querySelector("#cards").innerHTML =
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("switzerland", "07:00", "cameroon") +
+      createGame("uruguai", "10:00", "southkorea") +
+      createGame("portugal", "13:00", "ghana") +
+      createGame("brazil", "16:00", "serbia")
+  ) +
+  createCard(
+    "28/11",
+    "segunda",
+    createGame("cameroon", "07:00", "serbia") +
+      createGame("southkorea", "10:00", "ghana") +
+      createGame("brazil", "13:00", "switzerland") +
+      createGame("portugal", "16:00", "uruguai")
+  ) +
+  createCard(
+    "02/12",
+    "sexta",
+    createGame("southkorea", "12:00", "portugal") +
+      createGame("ghana", "12:00", "uruguai") +
+      createGame("serbia", "16:00", "switzerland") +
+      createGame("brazil", "16:00", "cameroon")
+  )
